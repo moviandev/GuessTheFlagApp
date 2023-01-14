85667a1c1201ac7412f619236f5fe37af1e0f7f5
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-14 00:08:19 -0300

    Add linear gradient

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,17 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            VStack(spacing: 0) {
-                Color.red
-                Color.blue
-            }
-            Text("Your content")
-                .foregroundStyle(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
-        }
-        .ignoresSafeArea()
+        LinearGradient(gradient: Gradient(stops: [.init(color: .white, location: 0.45), .init(color: .black, location: 0.55)]), startPoint: .top, endPoint: .bottom)
     }
 }
 
