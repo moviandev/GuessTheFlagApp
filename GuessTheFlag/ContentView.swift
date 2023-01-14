bda051afd45e95ab58acd6951ae3187173bd9930
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-14 00:12:54 -0300

    Add angularGradient

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        RadialGradient(gradient: .init(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+        AngularGradient(gradient: .init(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
     }
 }
 
