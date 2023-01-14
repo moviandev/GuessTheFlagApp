479bc67588ac01f9b696c901290d902787f831a1
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-14 00:22:57 -0300

    Add image using system files images

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,16 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Button {
-                print("Button was tapped")
-            } label: {
-                Text("Tap me!")
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(.red)
-            }
-        }
+        Image(systemName: "pencil")
     }
 }
 
