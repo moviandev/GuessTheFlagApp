7bbc0d42b55039279834d13642a50c88f37b4c27
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-13 23:46:40 -0300

    Using stacks to arrange our content

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,12 +10,22 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Text("H1")
+                Text("H2")
+                Text("H3")
+            }
+            HStack {
+                Text("Q1")
+                Text("Q2")
+                Text("Q3")
+            }
+            HStack {
+                Text("W1")
+                Text("W2")
+                Text("W3")
+            }
         }
-        .padding()
     }
 }
 
