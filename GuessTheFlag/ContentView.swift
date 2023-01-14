16d521c08d50c354f8e3b1887d4c440c6ef9770a
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-14 16:09:17 -0300

    Add @State to properties  like countries and correct answer,  add variables to hold scoreTtitle and score, create method to check if the correct flag is tapped and create func to ask the question again

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,9 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var totalScore = 0
     
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
         ZStack {
@@ -26,7 +30,7 @@ struct ContentView: View {
                 
                 ForEach(0..<3) {number in
                     Button {
-                        // flag tapped
+                        flagTapped(number)
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
@@ -34,6 +38,28 @@ struct ContentView: View {
                 }
             }
         }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your Score is \(totalScore)")
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            totalScore += 1
+        } else {
+            scoreTitle = "Wrong"
+            totalScore -= 1
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
