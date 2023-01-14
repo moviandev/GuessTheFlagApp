fcd70c05379f818c1b90299ed938f6965638d14d
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-14 00:45:17 -0300

    Rollback to initial state

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -11,16 +11,8 @@ struct ContentView: View {
     @State private var showingAlert = false
     
     var body: some View {
-        Button("Show alert") {
-            showingAlert = true
-        }
-        .alert("Important message", isPresented: $showingAlert) {
-            Button("Delete", role: .destructive) { }
-            Button("Cancel", role: .cancel) { }
-            Button("Ok") { }
-        } message: {
-            Text("Please read this")
-        }
+        Text("Hello, World!")
+            .padding()
     }
 }
 
