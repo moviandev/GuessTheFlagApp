75a16ec4606b60826c8e2eaf0949c00f0dc99133
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-14 00:20:12 -0300

    Style buttons

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,11 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Button("Delete selection", role: .destructive, action: executeDelete)
-    }
-    
-    func executeDelete() {
-        print("Now deleting")
+        VStack {
+            Button("Button 1") { }
+                .buttonStyle(.bordered)
+            Button("Button 2", role: .destructive) { }
+                .buttonStyle(.bordered)
+            Button("Button 3") { }
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+            Button("Button 4", role: .destructive) { }
+                .buttonStyle(.borderedProminent)
+        }
     }
 }
 
