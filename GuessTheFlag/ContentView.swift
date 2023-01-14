48fdf918228946792f0ee27dda937b9cb7e555a1
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-14 00:36:35 -0300

    Add customized buttons

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,12 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Button {
-                print("Edit button was tapped")
-            } label: {
-                Label("Edit", systemImage: "pencil")
-            }
+        Button {
+            print("Edit button was tapped")
+        } label: {
+            Label("Edit", systemImage: "pencil")
         }
     }
 }
