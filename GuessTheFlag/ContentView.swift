8e658318e0e44da6e9932bf358b346e4ded28a96
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-14 00:21:57 -0300

    Create own customized button

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,15 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Button("Button 1") { }
-                .buttonStyle(.bordered)
-            Button("Button 2", role: .destructive) { }
-                .buttonStyle(.bordered)
-            Button("Button 3") { }
-                .buttonStyle(.borderedProminent)
-                .tint(.mint)
-            Button("Button 4", role: .destructive) { }
-                .buttonStyle(.borderedProminent)
+            Button {
+                print("Button was tapped")
+            } label: {
+                Text("Tap me!")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(.red)
+            }
         }
     }
 }
